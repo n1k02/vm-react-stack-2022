2511c9d545a88fb67522f5a558444acf395b8b3a
n1k02 <ghostwarriorn@gmail.com>
2023-03-05 18:21:38 +0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FavoritesPage from "./pages/FavoritesPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <h1>
-      hello mf
-    </h1>
+      <>
+          <Navigation/>
+    <Routes>
+        <Route path={'/'} element={<HomePage/>}/>
+        <Route path={'/favorites'} element={<FavoritesPage/>}/>
+    </Routes>
+      </>
   );
 }
 
---
+++ src/pages/FavoritesPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoritesPage = () => {
+    return (
+        <div>
+            Fav
+        </div>
+    );
+};
+
+export default FavoritesPage;
